c50c32189bf93b153e29d2bad06eab2fe2194115
Kay <118884667+BadRix90@users.noreply.github.com>
2025-08-21 19:57:42 +0200

    add new chat area

--- src/app/features/chat/chat-area/chat-area.component.ts
+++ src/app/features/chat/chat-area/chat-area.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { NgFor } from '@angular/common';
+import { MessageInputComponent } from '../message-input/message-input.component';
 
 @Component({
   selector: 'app-chat-area',
-  imports: [],
+  imports: [MatCardModule, NgFor, MessageInputComponent],
   templateUrl: './chat-area.component.html',
   styleUrl: './chat-area.component.scss'
 })
 export class ChatAreaComponent {
-
+  currentChatName = 'Entwicklerteam';
+  memberCount = 3;
+  
+  // Mock Messages für erste Tests
+  mockMessages = [
+    {
+      id: '1',
+      user: 'Noah Braun',
+      time: '14:25',
+      content: 'Welche Version ist aktuell von Angular?',
+      avatar: '/assets/avatars/noah.jpg'
+    }
+  ];
 }
