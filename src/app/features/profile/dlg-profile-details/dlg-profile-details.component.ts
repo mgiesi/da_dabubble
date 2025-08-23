029cee40071539611522a6252bdacd507c62de5b
Markus Giesinger <mgiesi@gmail.com>
2025-08-23 23:38:25 +0200

    Add user status to profile comoponent and profile details dialog

--- src/app/features/profile/dlg-profile-details/dlg-profile-details.component.ts
+++ src/app/features/profile/dlg-profile-details/dlg-profile-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, Inject, inject, Input, Signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 import { User } from '../../../shared/models/user';
 import { NgIf } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -7,7 +8,7 @@ import { UsersFacadeService } from '../../../core/facades/users-facade.service';
 
 @Component({
   selector: 'app-dlg-profile-details',
-  imports: [MatDialogContent, NgIf],
+  imports: [MatDialogContent, NgIf, CommonModule],
   templateUrl: './dlg-profile-details.component.html',
   styleUrl: './dlg-profile-details.component.scss'
 })
