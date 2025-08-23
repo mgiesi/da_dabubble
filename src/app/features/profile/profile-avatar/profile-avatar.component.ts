b7430be73b2ef6170ad4656c3716feb795b8669a
Markus Giesinger <mgiesi@gmail.com>
2025-08-24 00:28:27 +0200

    Create a new component for the profile avatar (incl. user status)

---
+++ src/app/features/profile/profile-avatar/profile-avatar.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject, input, InputSignal, Signal } from '@angular/core';
+import { UsersFacadeService } from '../../../core/facades/users-facade.service';
+import { CommonModule } from '@angular/common';
+import { User } from '../../../shared/models/user';
+
+@Component({
+  selector: 'app-profile-avatar',
+  imports: [CommonModule],
+  templateUrl: './profile-avatar.component.html',
+  styleUrl: './profile-avatar.component.scss'
+})
+export class ProfileAvatarComponent {
+  facade = inject(UsersFacadeService);
+
+  /** Input variable for the Firebase user object which should be used with this component */
+  user: InputSignal<User | null> = input<User | null>(null);
+}
--- src/app/features/profile/profile-menu/profile-menu.component.ts
+++ src/app/features/profile/profile-menu/profile-menu.component.ts
@@ -7,10 +7,11 @@ import { Router } from '@angular/router';
 import { filter, firstValueFrom, take } from 'rxjs';
 import { DlgProfileMenuComponent } from '../dlg-profile-menu/dlg-profile-menu.component';
 import { MatDialog } from '@angular/material/dialog';
+import { ProfileAvatarComponent } from "../profile-avatar/profile-avatar.component";
 
 @Component({
   selector: 'app-profile-menu',
-  imports: [CommonModule],
+  imports: [CommonModule, ProfileAvatarComponent],
   templateUrl: './profile-menu.component.html',
   styleUrl: './profile-menu.component.scss'
 })
@@ -20,8 +21,12 @@ export class ProfileMenuComponent {
   router = inject(Router);
   dialog = inject(MatDialog);
 
+  /** Firebase User Object for the current logged in user */
   readonly user = toSignal(this.facade.currentUser(), { initialValue: null });
 
+  /**
+   * Sign out the current user and navigate back to the login page.
+   */
   async signOut() {
     await this.auth.signOut();
     await firstValueFrom(this.auth.user$.pipe(
