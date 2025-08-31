f38848f958b0eef60e63163b7c6666305b1f0aed
Markus Giesinger <mgiesi@gmail.com>
2025-08-31 09:46:44 +0200

    Add missing input validation for profile name

--- src/app/features/profile/dlg-profile-edit/dlg-profile-edit.component.ts
+++ src/app/features/profile/dlg-profile-edit/dlg-profile-edit.component.ts
@@ -1,5 +1,6 @@
 import { NgIf, CommonModule } from '@angular/common';
-import { Component, inject, Signal, WritableSignal } from '@angular/core';
+import { fadeInOut } from '../../../core/animations/fade-in-out.animation';
+import { Component, effect, inject, Signal, WritableSignal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { UsersFacadeService } from '../../../core/facades/users-facade.service';
 import { User } from '../../../shared/models/user';
@@ -9,7 +10,8 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-dlg-profile-edit',
   imports: [MatDialogContent, NgIf, CommonModule, FormsModule],
   templateUrl: './dlg-profile-edit.component.html',
-  styleUrl: './dlg-profile-edit.component.scss'
+  styleUrl: './dlg-profile-edit.component.scss',
+  animations: [fadeInOut],
 })
 export class DlgProfileEditComponent {
   dialogRef = inject(MatDialogRef<DlgProfileEditComponent>);
@@ -17,7 +19,19 @@ export class DlgProfileEditComponent {
 
   readonly user = inject<WritableSignal<User | null>>(MAT_DIALOG_DATA);
 
-  profileName: string = '';
+  fullName: string = '';
+  private hasInitializedFromuser = false;
+  customMinLengthError: boolean = false;
+
+  constructor() {
+    effect(() => {
+      const u = this.user();
+      if (!this.hasInitializedFromuser && u?.displayName) {
+        this.fullName = u.displayName;
+        this.hasInitializedFromuser = true;
+      }
+    })
+  }
 
   closeDialog() {
     this.dialogRef.close(false);
@@ -26,9 +40,14 @@ export class DlgProfileEditComponent {
   saveProfile() {
     const u = this.user();
     if (u) {
-      this.facade.updateDisplayName(u.id, this.profileName);
+      this.facade.updateDisplayName(u.id, this.fullName);
     }    
     this.dialogRef.close(false);
+  }
+
+  validateNameLength() {
+    const cleanedName = this.fullName.replace(/\s/g, '');
+    this.customMinLengthError = cleanedName.length < 4;
   }
 
 }
