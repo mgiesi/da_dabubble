3221751a6c42e22a2a2daab265df76d4263c0eae
Kay <118884667+BadRix90@users.noreply.github.com>
2025-09-02 20:59:23 +0200

    Align message footer and update reactions component
    Added conditional classes to align the message footer differently for own and other users' messages. Updated the message reactions component to accept an isOwnMessage input and improved reaction handling and tracking.

--- src/app/features/chat/message-item/message-reactions/message-reactions.component.ts
+++ src/app/features/chat/message-item/message-reactions/message-reactions.component.ts
@@ -3,19 +3,24 @@ import { NgFor, NgIf } from "@angular/common"
 
 @Component({
   selector: "app-message-reactions",
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf ],
   templateUrl: "./message-reactions.component.html",
   styleUrl: "./message-reactions.component.scss",
 })
 export class MessageReactionsComponent {
-  @Input() reactions: any[] = []
-  @Output() reactionClicked = new EventEmitter<any>()
+  @Input() reactions: { [emoji: string]: { count: number; users: string[] } } = {}
+  @Input() isOwnMessage = false // Add isOwnMessage input for positioning
+  @Output() reactionClicked = new EventEmitter<{ emoji: string; data: any }>()
 
-  onReactionClick(reaction: any) {
-    this.reactionClicked.emit(reaction)
+  onReactionClick(emoji: string, reactionData: any) {
+    this.reactionClicked.emit({ emoji, data: reactionData })
   }
 
-  trackByReaction(index: number, reaction: any): string {
-    return `${reaction.emoji}-${reaction.count}`
+  trackByReaction(index: number, item: any): string {
+    return item.key // emoji is the key
+  }
+
+  hasReactions(): boolean {
+    return this.reactions && Object.keys(this.reactions).length > 0
   }
 }
