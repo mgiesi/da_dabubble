3367e1655126cbf2be8f297980a357efe3a56baa
Jörg Bechtold <joergbechtold@gmx.net>
2025-09-18 11:00:06 +0200

    Refactor profile edit dialog to use ChooseAvatar
    Replaces the ProfileAvatar component with the ChooseAvatar component in the profile edit dialog. Updates related imports, HTML, and styles for improved avatar selection and layout consistency. Also refines button styling and layout in profile details and edit dialogs.

--- src/app/features/profile/dlg-profile-edit/dlg-profile-edit.component.ts
+++ src/app/features/profile/dlg-profile-edit/dlg-profile-edit.component.ts
@@ -1,16 +1,35 @@
 import { NgIf, CommonModule } from '@angular/common';
 import { fadeInOut } from '../../../core/animations/fade-in-out.animation';
-import { Component, effect, inject, Signal, WritableSignal } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import {
+  Component,
+  effect,
+  inject,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogContent,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { UsersFacadeService } from '../../../core/facades/users-facade.service';
 import { User } from '../../../shared/models/user';
 import { FormsModule } from '@angular/forms';
 import { DlgSelectAvatarComponent } from '../dlg-select-avatar/dlg-select-avatar.component';
 import { ProfileAvatarComponent } from '../profile-avatar/profile-avatar.component';
+import { ChooseAvatarComponent } from '../../auth/auth-assets/choose-avatar/choose-avatar.component';
 
 @Component({
   selector: 'app-dlg-profile-edit',
-  imports: [MatDialogContent, NgIf, CommonModule, FormsModule, ProfileAvatarComponent],
+  imports: [
+    MatDialogContent,
+    NgIf,
+    CommonModule,
+    FormsModule,
+
+    ChooseAvatarComponent,
+  ],
   templateUrl: './dlg-profile-edit.component.html',
   styleUrl: './dlg-profile-edit.component.scss',
   animations: [fadeInOut],
@@ -19,7 +38,7 @@ export class DlgProfileEditComponent {
   dialogRef = inject(MatDialogRef<DlgProfileEditComponent>);
   facade = inject(UsersFacadeService);
   private dialog = inject(MatDialog);
-  
+
   readonly user = inject<WritableSignal<User | null>>(MAT_DIALOG_DATA);
 
   fullName: string = '';
@@ -33,19 +52,17 @@ export class DlgProfileEditComponent {
         this.fullName = u.displayName;
         this.hasInitializedFromuser = true;
       }
-    })
+    });
   }
 
   editAvatar() {
     const current = this.user();
-    this.dialog.open(DlgSelectAvatarComponent, {     
-      data: structuredClone(current) as User | null
+    this.dialog.open(DlgSelectAvatarComponent, {
+      data: structuredClone(current) as User | null,
     });
     /*this.dialog.open(DlgSelectAvatarComponent, {
       data: this.user
     });*/
-
-  
   }
 
   closeDialog() {
@@ -56,7 +73,7 @@ export class DlgProfileEditComponent {
     const u = this.user();
     if (u) {
       this.facade.updateDisplayName(u.id, this.fullName);
-    }    
+    }
     this.dialogRef.close(false);
   }
 
@@ -64,5 +81,4 @@ export class DlgProfileEditComponent {
     const cleanedName = this.fullName.replace(/\s/g, '');
     this.customMinLengthError = cleanedName.length < 4;
   }
-
 }
